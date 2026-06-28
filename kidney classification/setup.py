4e90cca481e883b3d51e2ae66f925a6a73e4f3ff
Nandhakumar-2kk2 <skillnandha10@gmail.com>
2026-06-28 12:34:14 +0530

    base setup

---
+++ kidney classification/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Reads the requirements.txt file and returns
+    a list of required packages.
+    """
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="kidney_classification",
+    version="0.0.1",
+    author="Nandhakumar",
+    author_email="your_email@example.com",
+    description="End-to-End Deep Learning Kidney Disease Classification using CNN",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
