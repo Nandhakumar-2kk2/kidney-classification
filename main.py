3819a19943ab08a428d5b26878816f3a114e1634
Nandhakumar-2kk2 <skillnandha10@gmail.com>
2026-07-14 22:27:55 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-from src.config.configuration import ConfigurationManager
-from src.components.data_ingestion import DataIngestion
-from src.components.data_ingestion import DataIngestion
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
+from src.log.logger import logger
+
+STAGE_NAME="Data Ingestion Stage"
 
 
-def main():
-    config = ConfigurationManager()
+try:
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<')
+    obj=DataIngestionTrainigPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 
-    data_ingestion_config = config.get_data_ingestion_config()
-
-    data_ingestion = DataIngestion(config=data_ingestion_config)
-
-    data_ingestion.download_file()
-    data_ingestion.extract_zip_file()
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,30 @@
+from src.config.configuration import ConfigurationManager
+from src.components.data_ingestion import DataIngestion
+from src.log.logger import logger
+
+STAGE_NAME="Data Ingestion Stage"
+
+class DataIngestionTrainigPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+
+        data_ingestion_config = config.get_data_ingestion_config()
+
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__=="__main__":
+    try:
+        logger.info(f'>>>>> stage {STAGE_NAME} started <<<<')
+        obj=DataIngestionTrainigPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
